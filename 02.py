55eac9df14236334e0d83da983e8270ac2475743
Olexandrd <olexandrd@gmail.com>
2023-12-23 14:34:50 +0200

    Palindrome

---
+++ 02.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+
+def is_palindrom(text: str) -> bool:
+    dq = deque()
+    text = text.casefold()
+    text = text.replace(" ", "")
+    for i in text:
+        dq.append(i)
+    while len(dq) > 1:
+        right = dq.pop()
+        left = dq.popleft()
+        if right != left:
+            return False
+    return True
+
+
+def __main__():
+    print("Press Ctrl+C to exit")
+    while True:
+        print("Type your string>>>")
+        text = input()
+        if is_palindrom(text):
+            print(f'"{text}" is palindrome')
+        else:
+            print(f'"{text}" is not palindrome')
+
+
+if __name__ == "__main__":
+    __main__()
